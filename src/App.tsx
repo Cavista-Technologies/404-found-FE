d0db5849473cb0303a74d1666f008979c84a5540
Prosper Ikechukwu <pikechukwu@axxess.com>
2026-08-24 16:13:58 +0100

    feat: implemented tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Login } from "./pages/AuthPages/LoginPage";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="App">
+      <Routes>
+        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
