042e39b1be59bb347004e81d84da078dc6edd72f
dev-prosper99 <pikechukwu@axxess.com>
2026-08-31 04:22:51 +0100

    chore: removed the code for redundancy

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -71,7 +71,7 @@ function Input({
         onChange={handleChange}
         className={cn(
           "file:text-foreground placeholder:text-grey-500 selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-grey-300 w-full min-w-0 rounded-[10px] border bg-transparent px-3 py-3 text-sm text-grey-900 font-poppins transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
-          "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:border-primary-200",
+          "focus-visible:ring-ring/50 focus-visible:border-primary-200",
           "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
           icon && (isLeft ? "pl-11" : "pr-3"),
           className,
