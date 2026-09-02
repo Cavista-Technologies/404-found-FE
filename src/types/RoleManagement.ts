5de11d0d82200c2466a205f5b3f2fdd375bcad3a
dev-prosper99 <pikechukwu@axxess.com>
2026-09-02 23:23:37 +0100

    started the roles page

--- src/types/RoleManagement.ts
+++ src/types/RoleManagement.ts
@@ -3,3 +3,7 @@ import type z from "zod";
 
 export type CreateNewRoleFormInput = z.input<typeof CreateNewRoleSchema>
 export type CreateNewRoleFormOutput = z.output<typeof CreateNewRoleSchema>
+
+export interface RolesTableValues{
+    
+}
